3451538e6ed06ba2c4d423b74b54ab752af0e499
Yasin Murat Akyüz <ysnakyz55@gmail.com>
2021-12-14 13:38:34 +0300

    temel iskelet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //{ path: 'auth', component:AuthComponent },
+  //{ path: 'products/:slug', component: ProductDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
